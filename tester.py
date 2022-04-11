01f9de2fee24208268a2c806581eecd2ce097b2f
Timothy McNasby <mcnasbytim@gmail.com>
2022-04-11 14:34:26 -0400

    commit

--- tester.py
+++ tester.py
@@ -6,21 +6,21 @@ from random import randrange
 import matplotlib.pyplot as plt
 
 # Get Possible Guesses
-with open("words/words.txt", 'r') as f:
+with open("words/words.txt", "r") as f:
     words = f.readlines()
 for i, word in enumerate(words):
     words[i] = word.strip()
 
 # Get Posible Answers
-with open("words/answer_words.txt", 'r') as f:
-        answer_words = f.readlines()
+with open("words/answer_words.txt", "r") as f:
+    answer_words = f.readlines()
 for i, answer_word in enumerate(answer_words):
     answer_words[i] = answer_word.strip()
 
 # Variables
-runAmount = 1000
+runAmount = 50
 GUESSES = 6
-data = dict([(i,0) for i in range(1, GUESSES + 1)])
+data = dict([(i, 0) for i in range(1, GUESSES + 1)])
 data[-1] = 0
 
 for _ in range(0, runAmount):
@@ -30,14 +30,18 @@ for _ in range(0, runAmount):
     lateGame = False
     letterFreq, letterFreqLoc = GetFreqAndLocFreq(answer_words)
     needed_letters = GetMostCommonLetters(answer_words, letterFreq, letterFreqLoc)
-    
+
     for roundNum in range(1, GUESSES + 1):
         # Get geuss
         found_words = getAvalWords(words, needed_letters)
-        found_words_ranked = rankWords(found_words, letterFreq, letterFreqLoc, lateGame, answer_words, roundNum)
+        found_words_ranked = rankWords(
+            found_words, letterFreq, letterFreqLoc, lateGame, answer_words, roundNum
+        )
 
         # Sort ranked words
-        words_ranked_sorted = sorted(found_words_ranked.items(), key=lambda x: x[1], reverse=True)
+        words_ranked_sorted = sorted(
+            found_words_ranked.items(), key=lambda x: x[1], reverse=True
+        )
         try:
             words_sorted, values_sorted = map(list, zip(*words_ranked_sorted))
         except:
@@ -47,8 +51,10 @@ for _ in range(0, runAmount):
         geuss = words_sorted[0]
         result, done = compare(geuss, ANSWER)
 
-        needed_letters, letterFreq, letterFreqLoc = update(result, words_sorted[0], needed_letters, letterFreq, letterFreqLoc)
-                
+        needed_letters, letterFreq, letterFreqLoc = update(
+            result, words_sorted[0], needed_letters, letterFreq, letterFreqLoc
+        )
+
         if GUESSES == 3:
             lateGame = True
 
@@ -63,7 +69,7 @@ for _ in range(0, runAmount):
         # If Wrong
         else:
             if GUESSES == 1:
-                print(f'\nYou Failed :( the answer was {ANSWER}\n')
+                print(f"\nYou Failed :( the answer was {ANSWER}\n")
                 data[-1] += 1
             GUESSES -= 1
 
@@ -75,7 +81,7 @@ print(data)
 
 # Make Bar Graph
 plt.bar(roundLenSorted, valueSorted)
-plt.title('Amount of Attempts for Correct Answer')
-plt.xlabel('Amount of Attempts')
-plt.ylabel('Times this Occured')
+plt.title("Amount of Attempts for Correct Answer")
+plt.xlabel("Amount of Attempts")
+plt.ylabel("Times this Occured")
 plt.show()
