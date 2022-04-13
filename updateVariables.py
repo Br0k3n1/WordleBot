fa3cdae04ec65e58a0c91e566cd7981936c2c45e
Timothy McNasby <mcnasbytim@gmail.com>
2022-04-13 13:16:06 -0400

    commit

--- updateVariables.py
+++ updateVariables.py
@@ -1,13 +1,16 @@
 def update(result, chosen_word, needed_letters, freq_ranker, loc_ranker):
+    used_letters = []
     for i, c in enumerate(result):
-        if c == 'O':
+        if c == "O" and chosen_word[i] not in used_letters:
             if chosen_word[i] in needed_letters:
                 needed_letters.remove(chosen_word[i])
-        elif c == 'G':
+                used_letters.append(chosen_word[i])
+        elif c == "G" and chosen_word[i] not in used_letters:
             loc_ranker[chosen_word[i]][i] += 100000
-        elif c == 'Y':
+            used_letters.append(chosen_word[i])
+        elif c == "Y" and chosen_word[i] not in used_letters:
             freq_ranker[chosen_word[i]] += 10000
             loc_ranker[chosen_word[i]][i] += -100000
-    
-        
+            used_letters.append(chosen_word[i])
+
     return needed_letters, freq_ranker, loc_ranker
